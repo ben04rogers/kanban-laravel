5bb512c1921c0f3d267ec11dc3865bea13b5d09e
Ben Rogers <benrogers1299@outlook.com>
2025-09-18 19:35:09 +1000

    Setup db tables and models

---
+++ app/Models/Board.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Board extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'user_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function columns(): HasMany
+    {
+        return $this->hasMany(BoardColumn::class)->orderBy('position');
+    }
+
+    public function cards(): HasMany
+    {
+        return $this->hasMany(Card::class);
+    }
+}
---
+++ app/Models/Card.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Card extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'position',
+        'board_id',
+        'board_column_id',
+        'user_id',
+    ];
+
+    protected $casts = [
+        'position' => 'integer',
+    ];
+
+    public function board(): BelongsTo
+    {
+        return $this->belongsTo(Board::class);
+    }
+
+    public function column(): BelongsTo
+    {
+        return $this->belongsTo(BoardColumn::class, 'board_column_id');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
